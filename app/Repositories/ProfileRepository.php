f644661ef42e4dda287980c7533f3de8ac1aa4e6
DevEdgeMan <ivicagithub@gmail.com>
2018-05-09 17:55:01 +0200

    add functionality for ProfileRepository

--- app/Repositories/ProfileRepository.php
+++ app/Repositories/ProfileRepository.php
@@ -3,10 +3,36 @@
 namespace App\Repositories;
 
 use App\Profile;
+use App\User;
+use App\Friendship;
 
-class Profilerepository
+class ProfileRepository
 {
-    public function checkFriendShip() {
-        
+    private static $instance;
+
+    protected function __construct() {
+        //Do nothing
+    }
+
+    public static function instance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    public function getFriends($fr1, $fr2) {
+        $users = collect(new User);
+        foreach ($fr1 as $fr)
+        {
+            $users = $users->concat([$fr->userreceived]);
+        }
+        foreach ($fr2 as $fr)
+        {
+            $users = $users->concat([$fr->usersent]);
+        }
+        return $users->all();
     }
 }
